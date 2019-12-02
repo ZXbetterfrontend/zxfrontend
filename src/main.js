8e2b7f95248f71991d745bc682410b5bdf12e72b
nukebomb <nukebomb@sina.cn>
2019-12-02 21:21:24 +0800

    new template

--- src/main.js
+++ src/main.js
@@ -1,44 +1,43 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-import './assets/common.css'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import './assets/icons/iconfont.css'
-import Echarts from 'echarts'
-import axios from 'axios'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
-Vue.prototype.$axios = axios
-Vue.prototype.echarts = Echarts
-// 区域标号转中文
-Vue.prototype.$areaBelong = function (areaNum) {
-  switch (areaNum) {
-    case 'allcity':
-      return '全市'
-    case 510104:
-      return '锦江区'
-    case 510105:
-      return '青羊区'
-    case 510106:
-      return '金牛区'
-    case 510107:
-      return '武侯区'
-    case 510108:
-      return '成华区'
-    case 510109:
-      return '高新区'
-  }
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import store from './store'
+import router from './router'
+
+import '@/icons' // icon
+import '@/permission' // permission control
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online! ! !
+ */
+import { mockXHR } from '../mock'
+if (process.env.NODE_ENV === 'production') {
+  mockXHR()
 }
-Vue.use(Echarts)
-Vue.use(ElementUI)
+
+// set ElementUI lang to EN
+Vue.use(ElementUI, { locale })
+// 如果想要中文版 element-ui，按如下方式声明
+// Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  store,
+  render: h => h(App)
 })
