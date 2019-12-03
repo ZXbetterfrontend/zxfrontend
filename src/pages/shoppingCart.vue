ea57aab13e918db7b6f04905a2c533501ca321ce
langren1353 <1353464539@qq.com>
2019-12-03 11:08:14 +0800

    购物车修改

--- src/pages/shoppingCart.vue
+++ src/pages/shoppingCart.vue
@@ -4,15 +4,15 @@
 		<el-table-column type="selection" width="55" ></el-table-column>
 		<el-table-column type="index" label="序号" width="60"></el-table-column>
 		<el-table-column prop="order" label="订单号" width="100" :formatter="formatSex"></el-table-column>
-		<el-table-column prop="date" label="日期"></el-table-column>
 		<el-table-column prop="name" label="商品名"></el-table-column>
 		<el-table-column prop="number" label="数量"></el-table-column>
-      <el-table-column prop="prices" label="价格"></el-table-column>
+      <el-table-column prop="prices" label="价格(人民币)"></el-table-column>
 		<el-table-column prop="date" label="日期"></el-table-column>
       <el-table-column><el-button type="primary" round @click="choose(cell)">删除</el-button></el-table-column>
 
 	</el-table>
-    <div style="float: right">
+    <div style="float: right;padding-top: 20px">
+       <el-button type="success" round>购买</el-button>
       <el-button type="danger" round>批量删除</el-button>
     </div>
   </div>
@@ -26,21 +26,21 @@ export default {
         name: 'Zhangsan',
         number: 30,
         prices: 12345,
-        date:2020-5-5
+        date:'2020-5-5'
       },
         {
           order: 'dgf1234',
           name: 'Zhangsan',
           number: 30,
           prices: 12345,
-          date:2020-5-5
+          date:'2020-5-5'
         },
         {
           order: 'dgf1234',
           name: 'Zhangsan',
           number: 30,
           prices: 12345,
-          date:2020-5-5
+          date:'2020-5-5'
 
         },
         {
@@ -48,7 +48,7 @@ export default {
           name: 'Zhangsan',
           number: 30,
           prices: 12345,
-          date:2020-5-5
+          date:'2020-5-5'
 
         }]
     }
